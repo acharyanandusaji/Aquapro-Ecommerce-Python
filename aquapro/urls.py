b9f5467e3795d29d193ab648f5d7dd05c4774265
acharyanandusaji <acharyanandusaji@gmail.com>
2022-07-21 14:34:42 +0530

    Second Commit

--- aquapro/urls.py
+++ aquapro/urls.py
@@ -18,8 +18,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
